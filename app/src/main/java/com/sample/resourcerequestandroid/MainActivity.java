fd2435269d9ace0f11291597a5dcb3bfc4f32b9f
Shmulik Bardosh <shmulikb@shmulikb-mac.haifa.ibm.com>
2016-02-07 12:26:48 +0200

    getErrorMsg instead getErrorMsg

--- app/src/main/java/com/sample/resourcerequestandroid/MainActivity.java
+++ app/src/main/java/com/sample/resourcerequestandroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -22,12 +23,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView height = null;
     private TextView date = null;
     private static TextView summary = null;
+    private Button sendButtonm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         _this = this;
-        client = WLClient.createInstance(this);
+        WLClient.createInstance(this);
 
         setContentView(R.layout.activity_main);
         first_name = (TextView) findViewById(R.id.first_name);
@@ -40,59 +42,64 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
-    public void sendInfo(View view) {
-        try {
-            // Path Parameters (First Name, Middle Name and Last Name)
-            URI adapterPath = new URI("/adapters/JavaAdapter/users/"
-                    + first_name.getText().toString() +"/"
-                    + middle_name.getText().toString() +"/"
-                    + last_name.getText().toString()
-            );
+        sendButtonm = (Button) findViewById(R.id.button_send);
+        sendButtonm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // Path Parameters (First Name, Middle Name and Last Name)
+                    URI adapterPath = new URI("/adapters/JavaAdapter/users/"
+                            + first_name.getText().toString() + "/"
+                            + middle_name.getText().toString() + "/"
+                            + last_name.getText().toString()
+                    );
 
-            WLResourceRequest request = new WLResourceRequest(adapterPath,WLResourceRequest.POST);
+                    WLResourceRequest request = new WLResourceRequest(adapterPath, WLResourceRequest.POST);
+                    // Query Parameters
+                    request.setQueryParameter("age", age.getText().toString());
 
-            // Query Parameters
-            request.setQueryParameter("age", age.getText().toString());
+                    // Header Parameters
+                    request.addHeader("date", date.getText().toString());
 
-            // Header Parameters
-            request.addHeader("date", date.getText().toString());
+                    // Form Parameters
+                    HashMap formParams = new HashMap();
+                    formParams.put("height", height.getText().toString());
 
-            // Form Parameters
-            HashMap formParams = new HashMap();
-            formParams.put("height", height.getText().toString());
+                    // Send
+                    request.send(formParams, new WLResponseListener() {
+                        public void onSuccess(WLResponse response) {
+                            String responseText = response.getResponseText();
+                            String resultText = "";
 
-            // Send
-            request.send(formParams, new WLResponseListener(){
-                public void onSuccess(WLResponse response) {
-                    String responseText = response.getResponseText();
-                    String resultText = "";
+                            try {
+                                resultText += "Name = " + response.getResponseJSON().getString("first")
+                                        + " " + response.getResponseJSON().getString("middle")
+                                        + " " + response.getResponseJSON().getString("last") + "\n";
+                                resultText += "Age = " + response.getResponseJSON().getInt("age") + "\n";
+                                resultText += "Height = " + response.getResponseJSON().getString("height") + "\n";
+                                resultText += "Date = " + response.getResponseJSON().getString("Date");
+                            } catch (org.json.JSONException e) {
+                                _this.updateTextView(e.getMessage());
+                            }
 
-                    try{
-                        resultText += "Name = "+ response.getResponseJSON().getString("first")
-                                +" "+ response.getResponseJSON().getString("middle")
-                                +" "+ response.getResponseJSON().getString("last") +"\n";
-                        resultText += "Age = "+ response.getResponseJSON().getInt("age") +"\n";
-                        resultText += "Height = "+ response.getResponseJSON().getString("height") +"\n";
-                        resultText += "Date = "+ response.getResponseJSON().getString("Date");
-                    } catch(org.json.JSONException e){
-                        _this.updateTextView(e.getMessage());
-                    }
+                            Log.d("InvokeSuccess", responseText);
+                            _this.updateTextView(resultText);
+                        }
 
-                    Log.d("InvokeSuccess", responseText);
-                    _this.updateTextView(resultText);
+                        public void onFailure(WLFailResponse response) {
+                            //String responseText = response.getResponseText();
+                            String errorMsg = response.getErrorMsg();
+                            Log.d("InvokeFail", errorMsg);
+                            _this.updateTextView("Failed to Invoke Adapter Procedure\n" + errorMsg);
+                        }
+                    });
+
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
                 }
-                public void onFailure(WLFailResponse response) {
-                    String responseText = response.getResponseText();
-                    Log.d("InvokeFail", responseText);
-                    _this.updateTextView("Failed to Invoke Adapter Procedure\n"+ responseText);
-                }
-            });
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+            }
+        });
     }
 
     public static void updateTextView(final String str){
